6152a4f8531d78f016ef3cb55479b85e9d1a9c91
cs-mas <masako.gray@clearscore.com>
2022-10-11 18:00:57 +0100

    setting up button with onClickListener for secont activity page

--- app/src/main/java/com/practice/quizapp/MainActivity.kt
+++ app/src/main/java/com/practice/quizapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.practice.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_start : Button = findViewById(R.id.btn_start)
+        btn_start.setOnClickListener{
+
+        }
     }
 }
