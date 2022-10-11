4d4fc5a23e85baa1281b0471e7bc06c4ac7389e6
cs-mas <masako.gray@clearscore.com>
2022-10-11 18:09:38 +0100

    adding logic - when you click start button without input, message pops up

--- app/src/main/java/com/practice/quizapp/MainActivity.kt
+++ app/src/main/java/com/practice/quizapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.practice.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btn_start : Button = findViewById(R.id.btn_start)
+        val editTextNameField : AppCompatEditText = findViewById(R.id.EditText_name_field)
         btn_start.setOnClickListener{
-
+            if(editTextNameField.text.isEmpty()){
+                Toast.makeText(
+                    this,
+                    "please enter your name",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
