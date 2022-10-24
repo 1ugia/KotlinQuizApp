dbf1bd595e03087164f4f15d5950b7b8896a1d2d
cs-mas <masako.gray@clearscore.com>
2022-10-24 19:13:22 +0100

    assigning/creating valuables to type of UI

--- app/src/main/java/com/practice/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/practice/quizapp/QuizQuestionActivity.kt
@@ -3,8 +3,23 @@ package com.practice.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionActivity : AppCompatActivity() {
+
+    //        Importing IDs from the UI (xml) page
+    private var progressBar : ProgressBar? = null
+    private var textViewProgress : TextView? = null
+    private var textViewQuestion : TextView? = null
+    private var imageViewImage : ImageView? = null
+
+    private var textView_option1 : TextView? = null
+    private var textView_option2 : TextView? = null
+    private var textView_option3 : TextView? = null
+    private var textView_option4 : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
